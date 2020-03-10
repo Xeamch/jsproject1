172ba2f37498815099c3b5cba13030aedcab7911
Sergey Barmin <Xeamch@yandex.ru>
2020-03-10 23:08:25 +0300

    onehomework

--- project 1/js/script.js
+++ project 1/js/script.js
@@ -1,4 +1,23 @@
 'use strict';
 
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    b = prompt("Во сколько обойдется?");
+let с = prompt("Введите обязательную статью расходов в этом месяце"),
+    d = prompt("Во сколько обойдется?");
 
-console.log(3%2);
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+
+};
+
+appData.expenses.a = b;
+appData.expenses.c = d;
+alert(appData.budget/30);
